1fabbc56bb1ac01f2a814c6e38c7a5aebe816c73
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-05-22 04:22:52 -0700

    This is a beautiful code that passes the Betty checks

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository for The Simple Shell project
---
+++ beautiful.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print - Prints a statement
+ */
+
+void print(void)
+{
+printf("I am a beautiful code\n");
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+print();
+
+return (0);
+}
+
