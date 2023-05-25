1732a57cb2447a9a96a6352d3531c55ca3724c2a
ChaimaaFr <chaimaefarkouchi@gmail.com>
2023-05-25 11:32:02 -0700

    error fix

--- builtins_a.c
+++ builtins_a.c
@@ -5,22 +5,22 @@
  * @info: parameter struct
  * @str: the string alias
  *
- * Return:  Always 0 on success, 1 on error
+ * Return: Always 0 on success, 1 on error
  */
 int my_unset_alias(info_f *info, char *str)
 {
-char *p, c;
-int ret;
+	char *p, c;
+	int ret;
 
-p = my_strchr(str, '=');
-if (!p)
-return (1);
-c = *p;
-*p = 0;
-ret = my_delete_node_at_index(&(info->alias),
-my_node_index(info->alias, my_node_str_start(info->alias, str, -1)));
-*p = c;
-return (ret);
+	p = my_strchr(str, '=');
+	if (!p)
+		return (1);
+	c = *p;
+	*p = 0;
+	ret = my_delete_node_at_index(&(info->alias),
+							   my_node_index(info->alias, my_node_str_start(info->alias, str, -1)));
+	*p = c;
+	return (ret);
 }
 
 /**
@@ -28,74 +28,76 @@ return (ret);
  * @info: parameter struct
  * @str: the string alias
  *
- * Return: 0 (Success) 1 (Failure)
+ * Return: Always 0 on success, 1 on error
  */
 int my_set_alias(info_f *info, char *str)
 {
-char *p;
-p = my_strchr(str, '=');
-if (!p)
-return (1);
-if (!*++p)
-return (my_unset_alias(info, str));
+	char *p;
 
-my_unset_alias(info, str);
-return (my_add_node_end(&(info->alias), str, 0) == NULL);
+	p = my_strchr(str, '=');
+	if (!p)
+		return (1);
+	if (!*++p)
+		return (my_unset_alias(info, str));
+
+	my_unset_alias(info, str);
+	return (my_add_node_end(&(info->alias), str, 0) == NULL);
 }
 
 /**
  * my_print_alias - prints an alias string
  * @node: the alias node
  *
- * Return: 0 (Success) 1 (Error)
+ * Return: Always 0 on success, 1 on error
  */
 int my_print_alias(list_f *node)
 {
-char *p = NULL, *a = NULL;
-if (node)
-{
-p = my_strchr(node->str, '=');
-for (a = node->str; a <= p; a++)
-my_putchar(*a);
-my_putchar('\'');
-my_puts(p + 1);
-my_puts("'\n");
-return (0);
-}
-return (1);
+	char *p = NULL, *a = NULL;
+
+	if (node)
+	{
+		p = my_strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+			my_putchar(*a);
+		my_putchar('\'');
+		my_puts(p + 1);
+		my_puts("'\n");
+		return (0);
+	}
+	return (1);
 }
 
 /**
  * my_alias - mimics the alias builtin (man alias)
- * @info: contains simulated arguments for a function pointer
- *
- * Return: Always 0
+ * @info: contains simulated arguments for a function pointer,
+ * allowing for a consistent function prototype
+ *	Return: Always 0
  */
 int my_alias(info_f *info)
 {
-int i = 0;
-char *p = NULL;
-list_f *node = NULL;
+	int i = 0;
+	char *p = NULL;
+	list_f *node = NULL;
 
-if (info->argc == 1)
-{
-node = info->alias;
-while (node)
-{
-my_print_alias(node);
-node = node->next;
-}
-return (0);
-}
-for (i = 1; info->argv[i]; i++)
-{
-p = my_strchr(info->argv[i], '=');
-if (p)
-my_set_alias(info, info->argv[i]);
-else
-my_print_alias(my_node_str_start(info->alias, info->argv[i], '='));
-}
-return (0);
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			my_print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = my_strchr(info->argv[i], '=');
+		if (p)
+			my_set_alias(info, info->argv[i]);
+		else
+			my_print_alias(my_node_str_start(info->alias, info->argv[i], '='));
+	}
+	return (0);
 }
 
 /**
@@ -106,24 +108,24 @@ return (0);
  */
 int my_change_alias(info_f *info)
 {
-int i;
-list_f *node;
-char *p;
+	int i;
+	list_f *node;
+	char *p;
 
-for (i = 0; i < 10; i++)
-{
-node = my_node_str_start(info->alias, info->argv[0], '=');
-if (!node)
-return (0);
-free(info->argv[0]);
-p = my_strchr(node->str, '=');
-if (!p)
-return (0);
-p = my_strdup(p + 1);
-if (!p)
-return (0);
-info->argv[0] = p;
-}
-return (1);
-}
+	for (i = 0; i < 10; i++)
+	{
+		node = my_node_str_start(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = my_strchr(node->str, '=');
+		if (!p)
+			return (0);
+		p = my_strdup(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
 
+	return (1);
+}
--- entry_point.c
+++ entry_point.c
@@ -1,160 +1,197 @@
 #include "shell.h"
 
 /**
- * my_shell - main control for the shell program
- * @info: pointer to an info_f struct containing shell information
- * @av: array of strings containing arguments
+ * my_shell_main - main control for the shell program
+ * @info: pointer to an info_s struct containing shell information
+ * @av: array of strings containing arguments to the shell
  *
  * Return: the status of the last executed builtin command
  */
+int my_shell_main(info_f *info, char **av)
+{
+	ssize_t read_result = 0;
+	int builtin_return_value = 0;
 
-int my_shell(info_f *info, char **av)
-{
-ssize_t read_result = 0;
-int builtin_return_value = 0;
-while (read_result != -1 && builtin_return_value != -2)
-{
-my_clear_info(info);
-if (my_from_terminal(info))
-my_puts("OurShell> ");
-my_putchar_err(NEG_ONE);
-read_result = my_get_input(info);
-if (read_result != -1)
-{
-my_set_info(info, av);
-builtin_return_value = my_builtin(info);
-if (builtin_return_value == -1)
-my_check_command(info);
-}
-else if (my_from_terminal(info))
-my_putchar('\n');
-my_free_info(info, 0);
-}
-my_create_history(info);
-my_free_info(info, 1);
-if (!my_from_terminal(info) && info->status)
-exit(info->status);
-if (builtin_return_value == -2)
-{
-if (info->error_code == -1)
-exit(info->status);
-exit(info->error_code);
-}
-return (builtin_return_value);
+	while (read_result != -1 && builtin_return_value != -2)
+	{
+		my_clear_info(info);
+
+		/* Display the shell prompt if in interactive mode */
+		if (my_from_terminal(info))
+			my_puts("$ ");
+
+		my_putchar_err(NEG_ONE);
+		read_result = my_get_input(info);
+
+		/* Handle input if it was successfully read */
+		if (read_result != -1)
+		{
+			my_set_info(info, av);
+			builtin_return_value = my_builtin(info);
+
+			/* Check for command execution if not a builtin command */
+			if (builtin_return_value == -1)
+				my_check_command(info);
+		}
+
+		/* Handle end of input if in from_terminal mode */
+		else if (my_from_terminal(info))
+			my_putchar('\n');
+
+		my_free_info(info, 0);
+	}
+
+	/* Create and store the history list */
+	my_create_history(info);
+
+	/* Free memory and exit */
+	my_free_info(info, 1);
+	if (!my_from_terminal(info) && info->status)
+		exit(info->status);
+
+	/* Handle exit with error */
+	if (builtin_return_value == -2)
+	{
+		if (info->error_code == -1)
+			exit(info->status);
+		exit(info->error_code);
+	}
+
+	/* Return the last executed builtin command's status */
+	return (builtin_return_value);
 }
 
 /**
  * my_builtin - finds a builtin command
  * @info: the parameter & return info struct
  *
- * Return: -1 (Not found) 0 (Success) 1 (Failure) 2 (exit)
+ * Return: -1 if builtin not found,
+ * 0 if builtin executed successfully,
+ * 1 if builtin found but not successful,
+ * 2 if builtin signals exit()
  */
-
 int my_builtin(info_f *info)
 {
-int i, builtin_return_value = -1;
-builtin_cmds builtints[] = {
-{"cd", my_cd},
-{"env", my_printenv},
-{"exit", my_exit},
-{"help", my_help},
-{"alias", my_alias},
-{"setenv", my_check_setenv},
-{"history", my_history},
-{"unsetenv", my_check_unsetenv},
-{NULL, NULL}};
+	int i, builtin_return_value = -1;
 
-for (i = 0; builtints[i].type; i++)
-if (my_strcmp(info->argv[0], builtints[i].type) == 0)
-{
-info->lines++;
-builtin_return_value = builtints[i].func(info);
-break;
-}
-return (builtin_return_value);
+	builtin_cmds builtints[] = {
+		{"cd", my_cd},
+		{"env", my_printenv},
+		{"exit", my_exit},
+		{"help", my_help},
+		{"alias", my_alias},
+		{"setenv", my_check_setenv},
+		{"history", my_history},
+		{"unsetenv", my_check_unsetenv},
+		{NULL, NULL}};
+
+	for (i = 0; builtints[i].type; i++)
+		if (my_strcmp(info->argv[0], builtints[i].type) == 0)
+		{
+			info->lines++;
+			builtin_return_value = builtints[i].func(info);
+			break;
+		}
+	return (builtin_return_value);
 }
 
 /**
- * my_check_command - searches for a command in PATH
+ * my_check_command - searches for a command in PATH or the current directory
  * @info: a pointer to the parameter and return info struct
  *
- * Return: Nothing
+ * Return: void
  */
 void my_check_command(info_f *info)
 {
-char *path = NULL;
-int i, word_count;
-info->path = info->argv[0];
-if (info->lc_flag == 1)
-{
-info->lines++;
-info->lc_flag = 0;
-}
-for (i = 0, word_count = 0; info->arg[i]; i++)
-if (!my_is_delimiter(info->arg[i], " \t\n"))
-word_count++;
-if (!word_count)
-return;
-path = my_check_file_in_path(info, my_getenv(info, "PATH="), info->argv[0]);
-if (path)
-{
-info->path = path;
-my_create_process(info);
-}
-else
-{
-if ((my_from_terminal(info) || my_getenv(info, "PATH=") || info->argv[0][0] == '/') && my_is_executable(info, info->argv[0]))
-my_create_process(info);
-else if (*(info->arg) != '\n')
-{
-info->status = 127;
-my_print_error(info, "Permission denied\n");
-}
-}
+	char *path = NULL;
+	int i, word_count;
+
+	info->path = info->argv[0];
+	if (info->lc_flag == 1)
+	{
+		info->lines++;
+		info->lc_flag = 0;
+	}
+
+	/* Count the number of non-delimiter words in the argument */
+	for (i = 0, word_count = 0; info->arg[i]; i++)
+		if (!my_is_delimiter(info->arg[i], " \t\n"))
+			word_count++;
+
+	/* If there are no words, exit without doing anything */
+	if (!word_count)
+		return;
+
+	/* Check if the command is in the PATH variable */
+	path = my_check_file_in_path(info, my_getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		my_create_process(info);
+	}
+	else
+	{
+		/* Check if the command is in the current directory */
+		if ((my_from_terminal(info) || my_getenv(info, "PATH=") || info->argv[0][0] == '/') && my_is_executable(info, info->argv[0]))
+			my_create_process(info);
+		/* If the command is not found, print an error message */
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			my_print_error(info, "not found\n");
+		}
+	}
 }
 
 /**
  * my_create_process - forks a new process to run the command
- * @info: pointer to the parameter
+ * @info: pointer to the parameter & return info struct
  *
- * Return: Nothing
+ * This function forks a new process and runs the command specified by the
+ * @info->argv array. The new process runs in a separate memory space and
+ * shares environment variables with the parent process.
+ *
+ * Return: void
  */
-
 void my_create_process(info_f *info)
 {
-pid_t pid;
+	pid_t pid;
 
-pid = fork();
-if (pid == -1)
-{
-/* ERROR FUNCTION */
-perror("Error:");
-return;
-}
-/* Child process */
-if (pid == 0)
-{
-if (execve(info->path, info->argv, my_get_environ(info)) == -1)
-{
-my_free_info(info, 1);
-if (errno == EACCES)
-exit(126);
-exit(1);
-}
-}
-/* Parent process */
-else
-{
-wait(&(info->status));
-if (WIFEXITED(info->status))
-{
-/* Set return status to child's exit status */
-info->status = WEXITSTATUS(info->status);
+	/* Fork a new process */
+	pid = fork();
+	if (pid == -1)
+	{
+		/* TODO: PUT ERROR FUNCTION */
+		perror("Error:");
+		return;
+	}
 
-/* Print error message for permission denied errors */
-if (info->status == 126)
-my_print_error(info, "Permission denied\n");
-}
-}
-}
+	/* Child process: execute the command */
+	if (pid == 0)
+	{
+		/* Execute the command */
+		if (execve(info->path, info->argv, my_get_environ(info)) == -1)
+		{
+			/* Handle execve errors */
+			my_free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: PUT ERROR FUNCTION */
+	}
+	/* Parent process: wait for child process to finish */
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			/* Set return status to child's exit status */
+			info->status = WEXITSTATUS(info->status);
 
+			/* Print error message for permission denied errors */
+			if (info->status == 126)
+				my_print_error(info, "Permission denied\n");
+		}
+	}
+}
